d2806aae9c766199c5d92aafa6b6ada75c735bd1
Techniquei <66029763+Techniquei@users.noreply.github.com>
2022-10-25 03:09:49 +0300

    1

--- controller/controller.js
+++ controller/controller.js
@@ -14,15 +14,21 @@ class Controller{
     }
 
     async getOneUser(req, res){
-
+        const id = req.params.id
+        const user = await db.query('SELECT * FROM person where id = $1', [id])
+        res.json(user.rows[0])
     }
 
     async updateUser(req, res){
-
+        const {id, name, surname} = req.body
+        const user = await db.query('UPDATE person SET name = $1, surname = $2 WHERE id  = $3 RETURNING *', [name, surname, id])
+        res.json(user.rows[0])
     }
 
     async deleteUser(req, res){
-
+        const id = req.params.id
+        const user = await db.query('DELETE FROM person where id = $1', [id])
+        res.json(user.rows[0])
     }
 
 }
